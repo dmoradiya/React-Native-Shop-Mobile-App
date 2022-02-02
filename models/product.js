2aacd2b6b38761d4f74147a0b64cceee0097443e
dmoradiya <dharmesh@ualberta.ca>
2022-02-01 21:48:35 -0700

    Initial commit; Add folder structure;

---
+++ models/product.js
@@ -0,0 +1,10 @@
+class Product{
+    constructor(productId, ownerID, title, imageUrl, description, price) {
+        this.id = productId;
+        this.ownerID = ownerID;
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.price = price;
+    }
+}
