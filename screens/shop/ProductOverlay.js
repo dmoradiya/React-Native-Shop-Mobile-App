953a2085ea8730cbb52d46dc6abbb62bf1f7907e
dmoradiya <dharmesh@ualberta.ca>
2022-02-02 23:45:51 -0700

    rendered all products in product overlay

--- screens/shop/ProductOverlay.js
+++ screens/shop/ProductOverlay.js
@@ -1,14 +1,28 @@
 import React from 'react';
-import { FlatList, View, Text } from 'react-native';
+import { FlatList, View, Text,Image } from 'react-native';
 import { useSelector } from "react-redux";
 
-const ProductOverlay = () => {
-    const allProducts = useSelector((state) => {
-      console.log(state.productList);
-    });
-  return <View>
-
-  </View>;
+const ProductOverlay = (props) => {
+    const allProducts = useSelector(state => state.productList.allProduct);
+    console.log(allProducts);
+  return (
+    <FlatList
+      data={allProducts}
+      keyExtractor={(item, index) => {item.map(x=>x.id)}}
+      renderItem={(itemData) =>
+        itemData.item.map((data) => (
+          <View>
+            <Image
+              style={{ width: 300, height: 300 }}
+              source={{ uri: data.imageUrl }}
+            />
+            <Text>{data.price}</Text>
+            <Text>{data.title}</Text>
+          </View>
+        ))
+      }
+    />
+  );
 };
 
 export default ProductOverlay;
